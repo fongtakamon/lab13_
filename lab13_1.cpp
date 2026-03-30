d2c2dc7a00ae4578d1ee3fe2dc31fbbd20312b6a
fongtakamon <noofongka@gmail.com>
2026-03-31 05:03:33 +0700

    done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,33 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double X[],int N,double Y[]){
+    for(int i = 0; i < 6; i++){
+        Y[i] = 0;
+    }
+
+    double sum = 0;
+    double sumSq = 0;
+    double product = 1;
+    double sumReciprocal = 0;
+
+    Y[4] = X[0];
+    Y[5] = X[0];
+
+    for(int i = 0; i < N; i++){
+        sum += X[i];
+        sumSq += X[i] * X[i];
+        product *= X[i];
+        sumReciprocal += 1.0 / X[i];
+
+        if(X[i] > Y[4]) Y[4] = X[i];
+        if(X[i] < Y[5]) Y[5] = X[i];
+    }
+    Y[0] = sum / N;
+
+    Y[1] = sqrt((sumSq / N) - (Y[0] * Y[0]));
+
+    Y[2] = pow(product, 1.0 / N);
+
+    Y[3] = N / sumReciprocal;
+}
